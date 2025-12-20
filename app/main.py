9a025cb72837800e161af223094109ea092dfeab
ten4i <oleksijchunihin@gmail.com>
2025-12-20 08:08:14 +0200

    last fixes

--- app/main.py
+++ app/main.py
@@ -13,10 +13,10 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
-
+ 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float,
-                 clean_power: int, average_rating: float,
+                clean_power: int, average_rating: float,
                  count_of_ratings: int) -> None:
 
         if not (1.0 <= distance_from_city_center <= 10.0):
@@ -28,12 +28,12 @@ class CarWashStation:
         if not (1.0 <= average_rating <= 5.0):
             raise ValueError("average_rating must be between 1 and 5")
         if not isinstance(count_of_ratings, int) or count_of_ratings < 0:
-            raise ValueError("count_of_ratings must be int")
+            raise ValueError("count_of_ratingss must be int")
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_rating = count_of_ratings
+        self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
         price = (
@@ -45,20 +45,21 @@ class CarWashStation:
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> float:
-        incomes = 0.0
+        price = self.calculate_washing_price(car)
         car.clean_mark = self.clean_power
-        incomes += self.calculate_washing_price(car)
-        return incomes
+        return price
 
-    def serve_cars(self, cars: list[Car], incomes: float) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
+        incomes = 0.0
         for car in cars:
-            if self.clean_power < car.clean_mark:
-                raise ValueError("car_clean_mark > wash_clean_power")
+            if car.clean_mark >= self.clean_power:
+               continue 
             incomes += self.wash_single_car(car)
         return round(incomes, 1)
 
     def rate_service(self, rating: int) -> int:
+        total = self.average_rating * self.count_of_ratings
+        total += rating
         self.count_of_ratings += 1
-        rating += rating
-        self.average_rating = rating / self.count_of_ratings
+        self.average_rating = round(total / self.count_of_ratings, 1)
         return rating
